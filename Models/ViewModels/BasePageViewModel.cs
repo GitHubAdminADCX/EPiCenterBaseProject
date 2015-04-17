f2120275370f39998095c6db014ec3cb8440a8fa
sbhandwa <sameer.bhandwalkar@capgemini.com>
2015-04-17 12:32:02 +0200

    Changes to base classes

--- Models/ViewModels/BasePageViewModel.cs
+++ Models/ViewModels/BasePageViewModel.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Web;
 using EPiServer.Core;
 using EPiCenterBaseProject.Controllers;
+using EPiCenterBaseProject.Business.Interfaces;
+using EPiServer.ServiceLocation;
 
 namespace EPiCenterBaseProject.Models.ViewModels
 {
     public class BasePageViewModel
     {
+        private readonly IPageService _pageService = ServiceLocator.Current.GetInstance<IPageService>();
+
         public string Title { get; set; }
-        public PageData StartPage { get; set; }
+        public PageData StartPage { get; set;  }
         public HeaderViewModel HeaderViewModel { get; set; }
     }
 }
